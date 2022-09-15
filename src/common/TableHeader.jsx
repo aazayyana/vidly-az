60bfe5d0ead6e6f1057e97d727564686cb01e682
Abba Zayyana <aazayyana@gmail.com>
2022-09-15 21:29:24 +0100

    adding sort icon

--- src/common/TableHeader.jsx
+++ src/common/TableHeader.jsx
@@ -12,14 +12,21 @@ function TableHeader({ columns, onSort, sortColumn }) {
 		onSort(selectedColumn);
 	};
 
+	const renderSortIcon = (column) => {
+		if (column.path !== sortColumn.path) return null;
+		if (sortColumn.order === "asc") return <i className="fa fa-sort-asc" />;
+		return <i className="fa fa-sort-desc" />;
+	};
+
 	return (
 		<thead>
 			<tr>
 				{columns.map((column) => (
 					<th
+						className="clickable"
 						key={column.path || column.key}
 						onClick={() => raiseSort(column.path)}>
-						{column.label}
+						{column.label} {renderSortIcon(column)}
 					</th>
 				))}
 			</tr>
